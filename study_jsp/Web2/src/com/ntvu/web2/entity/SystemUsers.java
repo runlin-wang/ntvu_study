ccf73d43f37e4341ce13601ba67d393b104d21e9
alin <alin.run@foxmail.com>
2020-10-14 00:33:00 +0800

    fear(jsp/Web2/jdbc): add jdbc* 新增 roles、system_users 表对应的Java类

---
+++ study_jsp/Web2/src/com/ntvu/web2/entity/SystemUsers.java
@@ -0,0 +1,92 @@
+package com.ntvu.web2.entity;
+
+/**
+ * 数据库 system_users表 对应的java类
+ */
+public class SystemUsers {
+    private int id;
+    private String loginName;
+    private String loginPassword;
+    private String loginSalt;
+    private String telephone;
+    private String email;
+    private boolean status;
+    private int roleId;
+
+    /**
+     * 用户对应的角色
+     */
+    private Roles role;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getLoginName() {
+        return loginName;
+    }
+
+    public void setLoginName(String loginName) {
+        this.loginName = loginName;
+    }
+
+    public String getLoginPassword() {
+        return loginPassword;
+    }
+
+    public void setLoginPassword(String loginPassword) {
+        this.loginPassword = loginPassword;
+    }
+
+    public String getLoginSalt() {
+        return loginSalt;
+    }
+
+    public void setLoginSalt(String loginSalt) {
+        this.loginSalt = loginSalt;
+    }
+
+    public String getTelephone() {
+        return telephone;
+    }
+
+    public void setTelephone(String telephone) {
+        this.telephone = telephone;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public boolean isStatus() {
+        return status;
+    }
+
+    public void setStatus(boolean status) {
+        this.status = status;
+    }
+
+    public int getRoleId() {
+        return roleId;
+    }
+
+    public void setRoleId(int roleId) {
+        this.roleId = roleId;
+    }
+
+    public Roles getRole() {
+        return role;
+    }
+
+    public void setRole(Roles role) {
+        this.role = role;
+    }
+}
