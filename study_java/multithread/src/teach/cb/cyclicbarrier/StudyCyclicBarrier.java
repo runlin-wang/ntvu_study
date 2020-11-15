668bd023344bb483bed0c002633e1d41e93f1c35
alin <alin.run@foxmail.com>
2020-11-15 22:50:15 +0800

    perf(java/multithread): update some thread code

--- study_java/multithread/src/teach/cb/cyclicbarrier/StudyCyclicBarrier.java
+++ study_java/multithread/src/teach/cb/cyclicbarrier/StudyCyclicBarrier.java
@@ -1,4 +1,67 @@
 package teach.cb.cyclicbarrier;
 
-public class StrCyclicbarrier {
+import java.util.ArrayList;
+import java.util.concurrent.BrokenBarrierException;
+import java.util.concurrent.CyclicBarrier;
+
+class SyncThread extends Thread {
+
+    private CyclicBarrier cb;
+    private int[] data;
+    private static int MIN = Integer.MAX_VALUE;
+
+    public SyncThread(CyclicBarrier cb, int... data) {
+        this.cb = cb;
+        this.data = data;
+        if (data.length < MIN) {
+            MIN = data.length;
+        }
+    }
+
+    @Override
+    public void run() {
+        for (int i = 0; i < MIN; i++) {
+            CBAction.lst.add(data[i]);
+            try {
+                cb.await();
+            } catch (InterruptedException | BrokenBarrierException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+}
+
+class CBAction extends Thread {
+
+    public static ArrayList<Integer> lst = new ArrayList<>();
+
+    @Override
+    public void run() {
+        for (int n : lst) {
+            System.out.print(n + " ");
+        }
+        System.out.println();
+        lst.clear();
+    }
+}
+
+public class StudyCyclicBarrier {
+    public static void main(String[] args) {
+        final int THREAD_COUNTS = 10;
+        CyclicBarrier cb = new CyclicBarrier(THREAD_COUNTS, new CBAction());
+//        new SyncThread(cb, new int[]{1, 2, 3, 8}).start();
+//        new SyncThread(cb, 4, 5, 6).start();
+//        new SyncThread(cb, 7, 8, 9).start();
+
+        for (int i = 0; i < THREAD_COUNTS; i++) {
+            int[] data = new int[3];
+            int base = 10 * (i + 1);
+            for (int j = 0; j < data.length; j++) {
+                data[j] = base + (j + 1);
+            }
+
+            new SyncThread(cb, data).start();
+        }
+
+    }
 }
--- study_java/multithread/src/teach/cb/exchanger/StudyExchanger.java
+++ study_java/multithread/src/teach/cb/exchanger/StudyExchanger.java
@@ -1,4 +1,53 @@
 package teach.cb.exchanger;
 
+import java.util.concurrent.Exchanger;
+
+class Person {
+
+    private final int id;
+    private final String name;
+
+    public Person(int id, String name) {
+        this.id = id;
+        this.name = name;
+    }
+
+    @Override
+    public String toString() {
+        return "Person{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                '}';
+    }
+}
+
+class ExchangerThread extends Thread {
+
+    private final Exchanger<Person> exch;
+    private final Person person;
+    private final String threadName;
+
+    public ExchangerThread(Exchanger<Person> exch, Person person, String threadName) {
+        this.exch = exch;
+        this.person = person;
+        this.threadName = threadName;
+    }
+
+    @Override
+    public void run() {
+        try {
+            Person received = exch.exchange(person);
+            System.out.println(threadName + ": person: " + person + ", received: " + received);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+    }
+}
+
 public class StudyExchanger {
+    public static void main(String[] args) {
+        Exchanger<Person> exch = new Exchanger<>();
+        new ExchangerThread(exch, new Person(111, "Alice"), "ThreadA").start();
+        new ExchangerThread(exch, new Person(222, "Bob"), "ThreadB").start();
+    }
 }
