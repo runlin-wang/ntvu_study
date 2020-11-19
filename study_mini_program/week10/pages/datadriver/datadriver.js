498b35332c6561c349b6deb767701941b9bff6bb
alin <alin.run@foxmail.com>
2020-11-19 23:10:04 +0800

    feat(mini): 定时器作业 demo

--- study_mini_program/week10/pages/datadriver/datadriver.js
+++ study_mini_program/week10/pages/datadriver/datadriver.js
@@ -12,14 +12,22 @@ Page({
    * 生命周期函数--监听页面加载
    */
   onLoad: function (options) {
-
+    
   },
 
   /**
    * 生命周期函数--监听页面初次渲染完成
    */
   onReady: function () {
-
+    //TODO 定时器作业
+    setTimeout(
+      function () {
+        // var isOdd = this.data.isOdd;
+        const page = miniProgram.currentPage()
+        // const page = await miniProgram.currentPage();
+        console.log(page.data);
+        // this.setData({isOdd: });
+      }, 2000);
   },
 
   /**
