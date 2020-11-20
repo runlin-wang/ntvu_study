89b1d8da2ad54a6282cfd4766c9b29d2fa39e48d
alin <alin.run@foxmail.com>
2020-11-20 23:21:49 +0800

    feat(mini): 实现初步计算器功能

--- study_mini_program/week10/pages/calc/calc.js
+++ study_mini_program/week10/pages/calc/calc.js
@@ -5,7 +5,10 @@ Page({
    * 页面的初始数据
    */
   data: {
-    result: 0
+    result: "0",
+    temp: "0", 
+    op: "",
+    flag: true
   },
 
   /**
@@ -65,22 +68,112 @@ Page({
   },
 
   clickBtn: function (e) {
+    var calculate = function(num1, op, num2) {
+      let data;
+      num1 = parseFloat(num1);
+      num2 = parseFloat(num2);
+  
+      switch (op) {
+        case "+":
+          data = num1 + num2;
+          break;
+        case "-":
+          data = num1 - num2;
+          break;
+        case "*":
+          data = num1 * num2;
+          break;
+        case "/":
+          if (num2 !== 0)
+            data = num1 / num2;
+          else {
+            data = 0;
+            wx.wx.showToast({
+              title: '❌，除数不能为 0 ！',
+              duration: 1500,
+            });
+          }
+          break;
+      }
+
+      return data;
+    }
+
     let data = this.data.result;
-    const value = e.currentTarget.dataset.value;
-    console.log(value > 0 && value <= 9);
     
+    let tmp = this.data.temp;
+    let op = this.data.op;
+    let noNumFlag = this.data.flag;
+    const value = e.currentTarget.dataset.value;
+    
+    // console.log("value: " + value +", data: " + data + ", tmp: " + tmp + ", op: " + op);
+
     if (value >= 0 && value <= 9) {
-      if (data == '0') {
+      if (data == '0' || noNumFlag) {
         data = value;
       }
       else {
         data += '' + value;
       }
+      noNumFlag = false;
     } else {
+      noNumFlag = true;
       //TODO 具体实现 加减乘除小数点和其他的 运算符
       console.log(value);
+
+      switch (value) {
+        case "dot":
+          // 输入的值中不包含小数点
+          if (data.toString().indexOf(".") == -1)
+            data += ".";
+          break;
+        case "clear":
+          data = 0;
+          tmp = 0;
+          op = "+";
+          break;
+        case "negative":
+          data = -1 * data;
+          break;
+        case "back":
+          if (data.length > 1)
+            data.substr(0, data.length - 1);
+          else
+            data = 0;
+          break;
+        case "div":
+          data = calculate(tmp, op, data);
+          tmp = data;
+          op = "/";
+          break;
+        case "mul":
+          data = calculate(tmp, op, data);
+          tmp = data;
+          op = "*";
+          break;
+        case "add":
+          data = calculate(tmp, op, data);
+          tmp = data;
+          op = "+";
+          break;
+        case "sub":
+          data = calculate(tmp, op, data);
+          tmp = data;
+          op = "-";
+          break;
+        case "equ":
+          data = calculate(tmp, op, data);
+          tmp = 0;
+          op = "+";
+          break;
+      }
     }
 
-    this.setData({result:data});
+    this.setData({
+      result:data,
+      op:op,
+      temp:tmp,
+      flag:noNumFlag
+    });
   }
 })
