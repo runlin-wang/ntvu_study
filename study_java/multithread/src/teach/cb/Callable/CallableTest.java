33251fedd938d0f7f9705c6b8b726fbc87c0d72d
alin <alin.run@foxmail.com>
2020-11-22 19:56:55 +0800

    update 3 thread

--- study_java/multithread/src/teach/cb/Callable/CallableTest.java
+++ study_java/multithread/src/teach/cb/Callable/CallableTest.java
@@ -22,18 +22,60 @@ class Sum implements Callable<Integer> {
     }
 }
 
+class Factorial implements Callable<Long> {
 
+    private int stop;
+
+    public Factorial(int stop) {
+        this.stop = stop;
+    }
+
+    @Override
+    public Long call() throws Exception {
+        long result = 1;
+
+        while (stop > 0)
+            result *= stop--;
+
+        return result;
+    }
+}
+
+class Hypotenuse implements Callable<Double> {
+    private final double side1;
+    private final double side2;
+
+    public Hypotenuse(double side1, double side2) {
+        this.side1=side1;
+        this.side2=side2;
+    }
+
+    @Override
+    public Double call() throws Exception {
+        return Math.sqrt(side1 * side1 + side2 * side2);
+    }
+}
 
 public class CallableTest {
     public static void main(String[] args) {
-        Sum s = new Sum(100);
-        int result = 0;
+        Sum sum = new Sum(100);
+        Factorial factorial = new Factorial(10);
+        Hypotenuse hypotenuse = new Hypotenuse(3.0, 4.0);
+
+        int s = 0;
+        long f = 0;
+        double h = 0;
+
         try {
-            result = s.call();
+            s = sum.call();
+            f = factorial.call();
+            h = hypotenuse.call();
         } catch (Exception e) {
             e.printStackTrace();
         }
 
-        System.out.println(result);
+        System.out.println("sum: " + s);
+        System.out.println("factorial: " + f);
+        System.out.println("hypotenuse: " + h);
     }
 }
