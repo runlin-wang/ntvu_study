12955ee932be85bbb3ee50681d23c940d49d6b4e
alin <alin.run@foxmail.com>
2020-12-09 19:25:04 +0800

    feat(java): add multithread code

--- study_java/multithread/src/my/lanqiao/guosai/Main.java
+++ study_java/multithread/src/my/lanqiao/guosai/Main.java
@@ -1,7 +1,37 @@
-package my.lanqiao.guosai;/**
-* @Package my.lanqiao.guosai
-* @author Mr.lin
-* @date 2020/12/6 19:10
-* @version V1.0
-*/public class Main {
+package my.lanqiao.guosai;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.Arrays;
+import java.util.Scanner;
+
+/**
+ * @author Mr.lin
+ * @version V1.0
+ * @Package my.lanqiao.guosai
+ * @date 2020/12/6 19:10
+ */
+public class Main {
+    public static void main(String[] args) throws IOException {
+
+        Scanner in = new Scanner(new File("data.txt"));
+        int sum = 0;                            // 总和
+        int n = in.nextInt();                   // 数组 f 下标
+        int[] f = new int[n];                   // 数组
+        int a_b, cost = 0;                  // a_b 进来和讲解的时间总和
+        for (int i = 0; i < n; i++) {
+            a_b = in.nextInt() + in.nextInt();
+            sum += a_b;
+            f[i] = a_b + in.nextInt();
+        }
+
+        Arrays.sort(f);
+
+        for (int i = 0; i < n; i++) {
+            sum += (n - i - 1) * f[i];
+        }
+
+        System.out.println(sum);
+
+    }
 }
--- study_java/multithread/src/teach/cb/dushu/Main.java
+++ study_java/multithread/src/teach/cb/dushu/Main.java
@@ -1,7 +1,89 @@
-package teach.cb.dushu;/**
-* @Package teach.cb.dushu
-* @author Mr.lin
-* @date 2020/12/6 20:02
-* @version V1.0
-*/public class Main {
+package teach.cb.dushu;
+
+/**
+ * @author Mr.lin
+ * @version V1.0
+ * @Package teach.cb.dushu
+ * @date 2020/12/6 20:02
+ */
+public class Main {
+
+    private static String[] unitDict = new String[]{"", "拾", "佰", "仟", "", "万", "亿"};
+    private static String[] digitDict = new String[]{"零", "壹", "贰", "叁", "肆", "伍", "陆", "柒", "捌", "玖"};
+
+    public static int generateArray(int n, int[] d) {
+        int index = -1;
+        int highestBit = -1;
+        int r;
+        do {
+            r = n % 10;
+            d[++index] = r;
+            if (r != 0)
+                highestBit = index;
+        } while ((n /= 10) != 0);
+        return highestBit;
+    }
+
+//    public static int generatedArray(int a, int[] d) {
+//        int i;
+//        for (i = 0; a != 0; i++, a /= 10) {
+//            d[i] = a % 10;
+//        }
+//
+//        return i;
+//    }
+
+    public static void main(String[] args) {
+
+        int[] d = new int[16];
+        int n = 233403200;
+        int highestBit = generateArray(n, d);
+        StringBuilder sb = new StringBuilder();
+
+        int flag = 1;
+        int sectionFlag;
+        int section = highestBit / 4;
+        int start = highestBit; // 9
+
+        for (int j, i = section; i >= 0; i--) {
+            sectionFlag = 0;
+            for (j = start; j >= i * 4; j--) {
+                if (d[j] != 0) {
+                    if (flag == 0) {
+                        sb.append(digitDict[0]);
+                    }
+                    sb.append(digitDict[d[j]]).append(unitDict[j % 4]);
+                    sectionFlag = flag = 1;
+                } else {
+                    flag = 0;
+                }
+            }
+            start = j;
+            if (sectionFlag == 1)
+                sb.append(unitDict[i + 4]);
+            flag = 1;
+        }
+
+        System.out.println(sb);
+
+
+//        int n = 9;
+//        int[] d = new int[4];
+//        int highestBit = generatedArray(n, d);
+//        StringBuilder sb = new StringBuilder();
+//
+//        boolean flag = false;
+//        for (int i = highestBit; i >= 0; i--) {
+//            if (d[i] > 0) {
+//                if (flag)
+//                    sb.append(digitDict[0]);
+//                sb.append(digitDict[d[i]]).append(unitDict[i]);
+//                flag = false;
+//            } else {
+//                flag = true;
+//            }
+//        }
+//
+//        System.out.println(sb);
+    }
 }
