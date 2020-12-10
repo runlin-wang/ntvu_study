37b8c8fd0f1c432f22d529344235f2b2fa071eea
alin <alin.run@foxmail.com>
2020-12-10 13:11:43 +0800

    feat(mini): 添加微天气代码

--- study_mini_program/week10/pages/weather/weather.js
+++ study_mini_program/week10/pages/weather/weather.js
@@ -5,62 +5,138 @@ Page({
    * 页面的初始数据
    */
   data: {
+    weather: {
+      wendu: 18,
+      // ganmao: '昼夜温差较大，较易发生感冒，请适当增减衣服。体质较弱的朋友请注意防护。',
+      yesterday: {
+        date: '17 日 星期四',
+        type: '阴',
+        fx: '南风',
+        fl: '微风级',
+        low: '低温 8℃',
+        high: '高温 16℃'
+      },
+      forecast: [
+        {
+          date: '18 日 星期五',
+          type: '阴',
+          high: '高温 16℃',
+          low: '低温 8℃',
+          fengxiang: '南风',
+          fengli: '微风级'
+        }, {
+          date: '18 日 星期五',
+          type: '阴',
+          high: '高温 16℃',
+          low: '低温 8℃',
+          fengxiang: '南风',
+          fengli: '微风级'
+        }, {
+          date: '18 日 星期五',
+          type: '阴',
+          high: '高温 16℃',
+          low: '低温 8℃',
+          fengxiang: '南风',
+          fengli: '微风级'
+        }, {
+          date: '18 日 星期五',
+          type: '阴',
+          high: '高温 16℃',
+          low: '低温 8℃',
+          fengxiang: '南风',
+          fengli: '微风级'
+        }, {
+          date: '18 日 星期五',
+          type: '阴',
+          high: '高温 16℃',
+          low: '低温 8℃',
+          fengxiang: '南风',
+          fengli: '微风级'
+        }
+      ]
+    },
+    today: '2020-12-09',
+    city: '南通',           // 城市名称
+    inputCity: '',         // 输入查询的城市名称
+  }, 
 
+    /**
+     * 生命周期函数--监听页面加载
+     */
+    onLoad: function (options) {
+    this.setData({
+      today: util.formatTime(new Date()).split(' ')[0]    // 更新当前日期
+    });
+    const self = this;
+    wx.getLocation({
+      type: 'wgs84',
+      altitude: false,
+      success: (result) => {
+        request({
+          url: 'https://api.map.baidu.com/geocoder/v2/' +
+            '?ak=token&location=' +
+            result.latitude + ',' + result.longitude + '&output=json&pois=0',
+          data: {},
+          header: {
+            'Content-Type': 'application/json'
+          },
+          success: function (res) {
+            const city = res.addressComponent.city.replace('市', '');   // 城市名称
+            self.searchWeather(city);                                   // 查询指定城市的天气信息
+          }
+        })
+      },
+      fail: () => { },
+      complete: () => { }
+    });
   },
 
-  /**
-   * 生命周期函数--监听页面加载
-   */
-  onLoad: function (options) {
+  searchWeather: function (cityName) {
+    const self = this;
+    let request = wx.request({
+      url: 'http://wthrcdn.etouch.cn/weather_mini?city=' + cityName,
+      data: {},
+      header: { 'content-type': 'application/json' },
+      method: 'GET',
+      dataType: 'json',
+      responseType: 'text',
+      success: (result) => {
+        if (result.data.status == 1002) {   // 无此城市
+          wx.showModal({
+            title: '提示',
+            content: '输入的城市名称有误，请重新输入！',
+            showCancel: false,
+            success: (result) => {
+              self.setData({ inputCity: '' });
+            },
+            fail: () => { },
+            complete: () => { }
+          });
+        } else {
+          const weather = result.data.weather;        // 获取天气数据
 
+          for (let i = 0; i < weather.forecast.length; i++) {
+            const element = weather.forecast[i];
+            weather.forecast[i] = '  ' + element.replace('星期', '  星期');
+          }
+          self.setData({
+            city: cityName,     // 更新显示天气城市名称
+            weather: weather,   // 更新天气信息
+            inputCity: ''       // 清空查询输入框
+          });
+        }
+      },
+      fail: () => { },
+      complete: () => { }
+    });
   },
 
-  /**
-   * 生命周期函数--监听页面初次渲染完成
-   */
-  onReady: function () {
-
+  // 输入事件
+  inputing: function (e) {
+    this.setData({ inputCity: e.detail.value });
   },
-
-  /**
-   * 生命周期函数--监听页面显示
-   */
-  onShow: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面隐藏
-   */
-  onHide: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面卸载
-   */
-  onUnload: function () {
-
-  },
-
-  /**
-   * 页面相关事件处理函数--监听用户下拉动作
-   */
-  onPullDownRefresh: function () {
-
-  },
-
-  /**
-   * 页面上拉触底事件的处理函数
-   */
-  onReachBottom: function () {
-
-  },
-
-  /**
-   * 用户点击右上角分享
-   */
-  onShareAppMessage: function () {
-
+  // 搜索按钮
+  bindSearch: function () {
+    this.searchWeather(this.data.inputCity);
   }
 })
