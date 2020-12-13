4a0892516fefd035df85d88a26db0db778ffab64
alin <alin.run@foxmail.com>
2020-12-13 20:13:43 +0800

    perf(java): perf FileInputStream (add download method )

--- study_java/teach-cb/src/study/stream/FileInputStreamDemo.java
+++ study_java/teach-cb/src/study/stream/FileInputStreamDemo.java
@@ -1,8 +1,10 @@
 package study.stream;
 
-import java.io.FileInputStream;
-import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
 import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.net.URL;
 
 /**
  * @author Mr.lin
@@ -11,18 +13,29 @@ import java.io.IOException;
  * @date 2020/12/13 19:11
  */
 public class FileInputStreamDemo {
-    public static void main(String[] args) {
-        FileInputStream fin;
-//        System.out.println(System.getProperty("user.dir"));
+
+    static void download(String urlString, String filePath) {
         try {
-            fin = new FileInputStream("src/study/stream/FileInputStreamDemo.java");
-            int ch;
-            while ((ch = fin.read()) != -1) {
-                System.out.print((char) ch);
+            URL url = new URL(urlString);
+            InputStream in = url.openStream();
+            FileOutputStream fout = new FileOutputStream(filePath);
+            byte[] buf = new byte[32];
+            int readLength;
+
+            while ((readLength = in.read(buf)) != -1) {
+                fout.write(buf, 0, readLength);
+//                System.out.print(new String(buf, 0 , readLength));
             }
         } catch (IOException e) {
             e.printStackTrace();
         }
+    }
+
+    public static void main(String[] args) {
+
+//        download("https://webmanager1.ntvu.edu.cn/", "W:/ntvu_study/study_java/teach-cb/ntvu.html");
+//        download("https://webmanager1.ntvu.edu.cn/", "W:/ntvu_study/study_java/teach-cb/data/ntvu/ntvu.html");
+        download("https://attup.ntvu.edu.cn/uploadfile/files/20200831175257939.doc", "data/ntvu/20200831175257939.doc");
 
     }
 }
