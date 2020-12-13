5c8260e622995616a6d05b374e8bbb963ad2bf32
alin <alin.run@foxmail.com>
2020-12-13 20:53:41 +0800

    perf(java): perf FileInputStream (add for in range(10))

--- study_java/teach-cb/src/study/stream/FileInputStreamDemo.java
+++ study_java/teach-cb/src/study/stream/FileInputStreamDemo.java
@@ -3,7 +3,6 @@ package study.stream;
 import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
-import java.io.OutputStream;
 import java.net.URL;
 
 /**
@@ -19,12 +18,12 @@ public class FileInputStreamDemo {
             URL url = new URL(urlString);
             InputStream in = url.openStream();
             FileOutputStream fout = new FileOutputStream(filePath);
-            byte[] buf = new byte[32];
-            int readLength;
+            byte[] buf = new byte[512];
+            int len;
 
-            while ((readLength = in.read(buf)) != -1) {
-                fout.write(buf, 0, readLength);
-//                System.out.print(new String(buf, 0 , readLength));
+            while ((len = in.read(buf)) != -1) {
+                fout.write(buf, 0, len);
+//                System.out.print(new String(buf, 0 , len));
             }
         } catch (IOException e) {
             e.printStackTrace();
@@ -35,7 +34,19 @@ public class FileInputStreamDemo {
 
 //        download("https://webmanager1.ntvu.edu.cn/", "W:/ntvu_study/study_java/teach-cb/ntvu.html");
 //        download("https://webmanager1.ntvu.edu.cn/", "W:/ntvu_study/study_java/teach-cb/data/ntvu/ntvu.html");
-        download("https://attup.ntvu.edu.cn/uploadfile/files/20200831175257939.doc", "data/ntvu/20200831175257939.doc");
+//        download("https://attup.ntvu.edu.cn/uploadfile/files/20200831175257939.doc", "data/ntvu/20200831175257939.doc");
+
+        long start = System.currentTimeMillis();
+        for (int i = 0; i < 10; i++) {
+            download("https://webmanager1.ntvu.edu.cn/", "data/ntvu/ntvu" + i + ".html");
+            download("https://attup.ntvu.edu.cn/uploadfile/files/20200831175257939.doc", "data/ntvu/ntvu" + i + ".doc");
+            download("https://attup.ntvu.edu.cn/attachment/core/label/2018_03/05_15/422efb26e8d856e4.jpg", "data/ntvu/ntvu" + i + ".jpg");
+        }
+
+        System.out.println(System.currentTimeMillis() - start);
+//        String url = "https://attup.ntvu.edu.cn/uploadfile/files/20200831175257939.doc";
+//        url = url.substring(url.lastIndexOf(47));
+//        System.out.println(url);
 
     }
 }
