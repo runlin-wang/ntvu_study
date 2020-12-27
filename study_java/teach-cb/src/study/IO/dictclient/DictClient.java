6d4f72fbd5e42cbd92890592b89eee45bce78741
alin <alin.run@foxmail.com>
2020-12-27 20:22:36 +0800

    feat(java): add DictClient.java - 用 socket 实现 telnet 的翻译功能（英译日）

--- study_java/teach-cb/src/study/IO/dictclient/DictClient.java
+++ study_java/teach-cb/src/study/IO/dictclient/DictClient.java
@@ -1,50 +1,9 @@
 package study.IO.dictclient;
 
 import java.io.*;
-
-class Phone implements Serializable {
-
-    private String homePhone;
-    transient private String mobilePhone;
-
-    public Phone(String homePhone, String mobilePhone) {
-        this.homePhone = homePhone;
-        this.mobilePhone = mobilePhone;
-    }
-
-    @Override
-    public String toString() {
-        return "{" +
-                "homePhone='" + homePhone + '\'' +
-                ", mobilePhone='" + mobilePhone + '\'' +
-                '}';
-    }
-}
-
-class Person implements Serializable {
-
-    private int id;
-    private String name;
-    transient private String gender;
-    private Phone phone;
-
-    public Person(int id, String name, String gender, Phone phone) {
-        this.id = id;
-        this.name = name;
-        this.gender = gender;
-        this.phone = phone;
-    }
-
-    @Override
-    public String toString() {
-        return "study.IO.dictclient.Person{" +
-                "id=" + id +
-                ", name='" + name + '\'' +
-                ", gender='" + gender + '\'' +
-                ", phone=" + phone +
-                '}';
-    }
-}
+import java.net.Socket;
+import java.nio.charset.StandardCharsets;
+import java.util.Scanner;
 
 /**
  * @author Mr.lin
@@ -53,22 +12,55 @@ class Person implements Serializable {
  * @date 2020/12/19 18:48
  */
 public class DictClient {
-    public static void main(String[] args) {
 
-        byte[] buf = new byte[0];
-        try (ByteArrayOutputStream bout = new ByteArrayOutputStream();
-             ObjectOutputStream oos = new ObjectOutputStream(bout)) {
-            Person person = new Person(990101, "Alice", "girl", new Phone("6686688", "17778595680"));
-            oos.writeObject(person);
-            buf = bout.toByteArray();
+    final static String SERVER = "dict.org";
+    final static int PORT = 2628;
+    final static int TIMEOUT = 15_000;
+
+    public static void main(String[] args) {
+        try (Socket socket = new Socket(SERVER, PORT);
+             Writer writer = new BufferedWriter(new OutputStreamWriter(socket.getOutputStream(), StandardCharsets.UTF_8));
+             BufferedReader reader = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+             Scanner in = new Scanner(System.in)) {
+//            socket.setSoTimeout(TIMEOUT);
+
+            String word;
+            for (; ; ) {
+                System.out.println("Please enter a word: (e or E to exit)");
+                word = in.next();
+                if ("e".equalsIgnoreCase(word.trim())) break;
+                define(word, writer, reader);
+            }
+
+            System.out.println("end!");
         } catch (IOException e) {
             e.printStackTrace();
         }
+    }
 
-        try (ObjectInputStream ois = new ObjectInputStream(new ByteArrayInputStream(buf))) {
-            Person person = (Person) ois.readObject();
-            System.out.println(person);
-        } catch (IOException | ClassNotFoundException e) {
+    static void define(String word, Writer writer, BufferedReader reader) {
+        try {
+            writer.write("define fd-eng-jpn " + word + "\r\n");
+            writer.flush();
+            String line;
+
+            while ((line = reader.readLine()) != null) {
+                if (line.startsWith("250")) break;
+                else if (line.startsWith("552")) {
+                    System.out.println("No definition found for " + word);
+                    break;
+                }
+                else if (line.startsWith("501")) {
+                    System.out.println("syntax error, illegal parameters");
+                    break;
+                } else if (line.startsWith("500")) {
+                    System.out.println("unknown command");
+                    break;
+                } else if (line.matches("\\d\\d\\d .*") || line.trim().endsWith(".")) continue;
+
+                System.out.println(line);
+            }
+        } catch (IOException e) {
             e.printStackTrace();
         }
     }
